b7d283521f515de5d4a6a713794b072217472c85
912-Crisan-Andrei <andrei.ionut.crisan2@stud.ubbcluj.ro>
2023-05-04 12:11:43 +0300

    all populations

--- backend-project/populate_visited.py
+++ backend-project/populate_visited.py
@@ -16,11 +16,11 @@ if __name__ == '__main__':
     visited_countries_file = open('visited_countries_insert.txt', 'w')
     visited_countries_file.write('INSERT INTO restapi_visitedcountries (individual_id, country_id, year, review) VALUES\n')
     n_visited_countries = 100000
-    for i in range(n_visited_countries):
+    for i in range(1, n_visited_countries+1):
         year = random.randint(2000, 2022)
         review = fake.sentence(nb_words=10)
-        values_str = f"({i}, {100000}, {year}, '{review}')"
-        if i == n_visited_countries - 1:
+        values_str = f"({i}, {100105}, {year}, '{review}')"
+        if i == n_visited_countries:
             values_str += ';'
         else:
             values_str += ',\n'
