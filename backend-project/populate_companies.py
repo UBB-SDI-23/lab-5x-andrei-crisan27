2d3463356baaa3f01c22e30c1d419401e4aa5684
912-Crisan-Andrei <andrei.ionut.crisan2@stud.ubbcluj.ro>
2023-05-04 12:31:11 +0300

    companies populate + text field

--- backend-project/populate_companies.py
+++ backend-project/populate_companies.py
@@ -1,9 +1,3 @@
-# name = models.CharField(max_length=25)
-# year_founded = models.IntegerField(validators=[validate_year_founded])
-# number_of_employees = models.IntegerField()
-# country = models.ForeignKey(Countries, on_delete=models.CASCADE)
-# activity = models.CharField(max_length=50)
-
 import os
 import random
 
@@ -18,19 +12,20 @@ from faker import Faker
 
 if __name__ == '__main__':
     fake = Faker()
-    n = 100000
+    n = 50000
     file = open('companies_insert.txt', 'w')
-    file.write('INSERT INTO restapi_companies (name, year_founded, number_of_employees, country_id, activity) VALUES\n')
+    file.write('INSERT INTO restapi_companies (name, year_founded, number_of_employees, country_id, activity, text_field) VALUES\n')
 
     for i in range(n):
         company_name = f"{fake.company()} {random.randint(1, 100)}"[:25]
         company_name = company_name.replace("'", "")
         year_founded = fake.random_int(min=1900, max=2022)
         number_of_employees = fake.random_int(min=1, max=10000)
-        country_id = random.randint(1, 1000000)
+        country_id = random.randint(1, 200000)
         activity = fake.word(ext_word_list=['Consulting', 'Technology', 'Manufacturing', 'Retail', 'Finance', 'Transportation'])[:50]
+        text_field = ' '.join([fake.word(ext_word_list=None, nb_chars=8) for _ in range(100)])
 
-        values_str = f"('{company_name}', {year_founded}, {number_of_employees}, {country_id}, '{activity}')"
+        values_str = f"('{company_name}', {year_founded}, {number_of_employees}, {country_id}, '{activity}', '{text_field}')"
         if i == n - 1:
             values_str += ';'
         else:
